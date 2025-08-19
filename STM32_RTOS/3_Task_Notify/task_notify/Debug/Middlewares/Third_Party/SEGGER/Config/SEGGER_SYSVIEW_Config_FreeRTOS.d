db03d1b1c91d33f849c3238ee275855a8a2a3919
Preethidhanavel <27preethid@gmail.com>
2025-08-19 18:50:57 +0530

    RTOS

---
+++ STM32_RTOS/3_Task_Notify/task_notify/Debug/Middlewares/Third_Party/SEGGER/Config/SEGGER_SYSVIEW_Config_FreeRTOS.d
@@ -0,0 +1,22 @@
+Middlewares/Third_Party/SEGGER/Config/SEGGER_SYSVIEW_Config_FreeRTOS.o: \
+ ../Middlewares/Third_Party/SEGGER/Config/SEGGER_SYSVIEW_Config_FreeRTOS.c \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
+ E:/bitsilica/STM32_RTOS_LOCAL/task_notify/Middlewares/Third_Party/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ E:/bitsilica/STM32_RTOS_LOCAL/task_notify/Middlewares/Third_Party/SEGGER/SEGGER/SEGGER.h \
+ E:/bitsilica/STM32_RTOS_LOCAL/task_notify/Middlewares/Third_Party/SEGGER/Config/Global.h
+../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
+../Core/Inc/FreeRTOSConfig.h:
+../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
+E:/bitsilica/STM32_RTOS_LOCAL/task_notify/Middlewares/Third_Party/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+E:/bitsilica/STM32_RTOS_LOCAL/task_notify/Middlewares/Third_Party/SEGGER/SEGGER/SEGGER.h:
+E:/bitsilica/STM32_RTOS_LOCAL/task_notify/Middlewares/Third_Party/SEGGER/Config/Global.h:
---
+++ STM32_RTOS/3_Task_Notify/task_notify/Debug/Middlewares/Third_Party/SEGGER/SEGGER/SEGGER_RTT.d
@@ -0,0 +1,6 @@
+Middlewares/Third_Party/SEGGER/SEGGER/SEGGER_RTT.o: \
+ ../Middlewares/Third_Party/SEGGER/SEGGER/SEGGER_RTT.c \
+ ../Middlewares/Third_Party/SEGGER/SEGGER/SEGGER_RTT.h \
+ E:/bitsilica/STM32_RTOS_LOCAL/task_notify/Middlewares/Third_Party/SEGGER/Config/SEGGER_RTT_Conf.h
+../Middlewares/Third_Party/SEGGER/SEGGER/SEGGER_RTT.h:
+E:/bitsilica/STM32_RTOS_LOCAL/task_notify/Middlewares/Third_Party/SEGGER/Config/SEGGER_RTT_Conf.h:
